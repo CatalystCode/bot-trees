f18e3ad4032e9d0deb9545b322447e12b5833933
amitu <ami.turgman@gmail.com>
2016-08-09 01:03:06 +0300

    adding basic dialog

--- index.js
+++ index.js
@@ -19,6 +19,29 @@ intents.onDefault([
 
 bot.dialog('/', intents);
 
+
+intents.matches(/^(help|hi|hello)/i, [
+    function (session) {
+        session.send('hello! help message etc... :-)');
+    }
+]);
+
+intents.matches(/^scenario1/i, [
+    function (session) {
+       builder.Prompts.confirm(session, 'confirm?', { listStyle: builder.ListStyle.button });
+    },
+    function (session, result) {
+        var answer = result.response;
+        if (answer) {
+          session.send('you said yes!');
+        } else {
+          session.send('you said no... :/');
+        }
+
+        session.endDialog();
+    }
+]);
+
 app.post('/api/messages', connector.listen());
 
 app.listen(port, function () {
