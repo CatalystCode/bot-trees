1f174c528530e7626b89823f40e20eaa5ef96192
morsh <mor.shemesh@gmail.com>
2016-08-08 18:07:54 -0700

    while parent on next step

--- findNextStep.js
+++ findNextStep.js
@@ -3,10 +3,10 @@ var conditionHandler = require('./conditionHandler');
 /**
  * session - bot session variable
  * tree - full json scenario template
- * current - pointer to current node in the scenario tree
  */
-module.exports = function findNextStep(session, tree, current) {
+module.exports = function findNextStep(session, tree) {
   var next = null;
+  var current = tree._nodeIds[session.currentNodeId];
 
   // If there are child scenarios, see if one of them answers a condition
   // In case it is, choose the first step in that scenario to as the next step
@@ -16,21 +16,17 @@ module.exports = function findNextStep(session, tree, current) {
       var scenario = current.scenarios[scenarioIndex];
       if (conditionHandler.evaluateExpression(session, scenario.condition)) {
 
-        next = (scenario.nodeId && tree.nodesIds[scenario.nodeId]) || scenario.steps[0];
+        next = (scenario.nodeId && tree._nodesIds[scenario.nodeId]) || scenario.steps[0];
         break;
       }
     }
   }
 
   // If there is no selected scenario
-  if (!next) {
-    if (current.next) {
-      next = current.next;
-    } 
-    
-    else if (current.parent && current.parent.next) {
-      next = current.parent.next;
-    }
+  var _node = current;
+  while (!next && _node) {
+    next = _node.next;
+    _node = _node.parent;
   }
 
   return next;
