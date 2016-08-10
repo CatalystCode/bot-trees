b68c4240f6184376741eb3f72f693485cc7a21d5
amitu <ami.turgman@gmail.com>
2016-08-10 03:09:27 +0300

    wip

--- handlers/summary.js
+++ handlers/summary.js
@@ -1,5 +1,5 @@
 
-module.exports = function (session, next) {
+module.exports = function (session, results, next) {
   var summary = "Summary: ";
   for (var prop in session.dialogData) {
     summary += prop + ': [' + session.dialogData[prop] + ']; '; 
