2823c8b9c52a7e3b59512507d12f20f7d212b7e2
amitu <ami.turgman@gmail.com>
2016-10-20 13:24:10 -0700

    implementing loading scenarios from external datasource

--- bot/index.js
+++ bot/index.js
@@ -19,6 +19,10 @@ var intents = new builder.IntentDialog();
 
 module.exports = connector;
 
+var scenariosPath = path.join(__dirname, 'scenarios');
+var handlersPath = path.join(__dirname, 'handlers');
+
+
 bot.dialog('/', intents);
 
 intents.matches(/^(help|hi|hello)/i, [
@@ -27,45 +31,80 @@ intents.matches(/^(help|hi|hello)/i, [
   }
 ]);
 
-// ============================================
+// dynamically load dialogs from external datasource
+// create a GraphDialog for each and bind it to the intents object
+loadDialogs()
+  .then(dialogs => {
+    for (var i=0; i<dialogs.length; i++) {
+  
+      ((dialog) => {
+        console.log(`loading scenario: ${dialog.scenario} for regex: ${dialog.regex}`);
+        
+        var dialogPath = `/${dialog.scenario}`;
+        var re = new RegExp(dialog.regex, 'i');
+        intents.matches(re, [
+          function (session) {
+            session.beginDialog(dialogPath, {});
+          }
+        ]);
 
-var scenariosPath = path.join(__dirname, 'scenarios');
-var handlersPath = path.join(__dirname, 'handlers');
+        GraphDialog
+          .fromScenario({ 
+            bot,
+            scenario: dialog.scenario, 
+            loadScenario, 
+            loadHandler,
+            customTypeHandlers: getCustomTypeHandlers()
+          })
+          .then(graphDialog => {
+            intents.onDefault(graphDialog.getDialog());
+            bot.dialog(dialogPath, graphDialog.getDialog());
+            console.log(`graph dialog loaded successfully: scenario ${dialog.scenario} for regExp: ${dialog.regex}`);
+          })
+          .catch(err => { console.error(`error loading dialog: ${err.message}`); });
+      })(dialogs[i]);
+    }
+  })
+  .catch(err => console.error(`error loading dialogs dynamically: ${err.message}`));
 
-
+/*
 // get a GraphDialog instance from a scenario asynchronously
 // when ready, attach the steps to the relevant intent dialog
 // provide 2 callbacks (loadScenario and loadHandler) that returns a Promise 
 // for loading scenario json and handlers from an external data source-
 // this can be from a file (as demonstrated below), a storage, etc.
 GraphDialog
-  .fromScenario( { 
+  .fromScenario({ 
       bot,
       scenario: 'router', 
       loadScenario, 
       loadHandler,
-
-      // this allows you to extend the json with more custom node types, 
-      // by providing your implementation to processing each custom type.
-      // in the end of your implemention you should call the next callbacks
-      // to allow the framework to continue with the dialog.
-      // refer to the customTypeStepDemo node in the stomachPain.json scenario for an example.
-      customTypeHandlers: [
-        {
-          name: 'myCustomType',
-          execute: (session, next, data) => {
-            console.log(`in custom node type handler: customTypeStepDemo, data: ${data.someData}`);
-            return next();
-          }
-        }
-      ]
-
+      customTypeHandlers: getCustomTypeHandlers()
     })
   .then(graphDialog => {
     intents.onDefault(graphDialog.getDialog());
     console.log('graph dialog loaded successfully');
   })
   .catch(err => { console.error(`error loading dialog`); });
+*/
+
+
+// this allows you to extend the json with more custom node types, 
+// by providing your implementation to processing each custom type.
+// in the end of your implemention you should call the next callbacks
+// to allow the framework to continue with the dialog.
+// refer to the customTypeStepDemo node in the stomachPain.json scenario for an example.
+function getCustomTypeHandlers() {
+  return [
+    {
+      name: 'myCustomType',
+      execute: (session, next, data) => {
+        console.log(`in custom node type handler: customTypeStepDemo, data: ${data.someData}`);
+        return next();
+      }
+    }
+  ];
+}
 
 // this is the handler for loading scenarios from external datasource
 // in this implementation we're just reading it from a file
@@ -77,7 +116,7 @@ function loadScenario(scenario) {
     // in this example we're loading from local file
     var scenarioPath = path.join(scenariosPath, scenario + '.json');
     
-    return fs.readFile(scenarioPath, 'utf8', function(err, content) {
+    return fs.readFile(scenarioPath, 'utf8', (err, content) => {
       if (err) {
         console.error("error loading json: " + scenarioPath);
         return reject(err);
@@ -86,7 +125,7 @@ function loadScenario(scenario) {
       var scenarioObj = JSON.parse(content);
 
       // simulating long load period
-      setTimeout(function() {
+      setTimeout(() => {
         console.log('resolving scenario', scenarioPath);
         resolve(scenarioObj);
       }, Math.random() * 3000);
@@ -106,15 +145,40 @@ function loadHandler(handler) {
     // in this example we're loading from local file
     var handlerPath = path.join(handlersPath, handler);
     var handlerString = null;
-    return fs.readFile(handlerPath, 'utf8', function(err, content) {
+    return fs.readFile(handlerPath, 'utf8', (err, content) => {
       if (err) {
         console.error("error loading handler: " + handlerPath);
         return reject(err);
       }
       // simulating long load period
-      setTimeout(function() {
+      setTimeout(() => {
         console.log('resolving handler', handler);
         resolve(content);
+      }, Math.random() * 3000);
+    });  
+  });
+}
+
+// this is the handler for loading scenarios from external datasource
+// in this implementation we're just reading it from the file scnearios/dialogs.json
+// but it can come from any external datasource like a file, db, etc.
+function loadDialogs() {
+  return new Promise((resolve, reject) => {
+    console.log('loading dialogs');
+
+    var dialogsPath = path.join(scenariosPath, "dialogs.json");
+    return fs.readFile(dialogsPath, 'utf8', (err, content) => {
+      if (err) {
+        console.error("error loading json: " + dialogsPath);
+        return reject(err);
+      }
+
+      var dialogs = JSON.parse(content);
+
+      // simulating long load period
+      setTimeout(() => {
+        console.log('resolving dialogs', dialogsPath);
+        resolve(dialogs.dialogs);
       }, Math.random() * 3000);
     });  
   });
